520733131ece2a47e3aecac6691f33fa1dd787ec
Jianxin <jianxinz233@gmail.com>
2025-03-24 17:22:30 +0100

    w1 layout and footer part

--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -1,15 +1,18 @@
-"use client"
+"use client";
 
-import { usePathname } from 'next/navigation';
-import styles from './Footer.module.css';
+import { usePathname } from "next/navigation";
+import styles from "./Footer.module.css";
 
 export const Footer = () => {
-  const path = usePathname().split('?')[0];
+  const path = usePathname().split("?")[0];
   return (
     <footer className={path !== "/" ? styles.footer : styles.hidden}>
       <div className={styles.footerDescription}>
         <h3>Galactica</h3>
-        <p>Explore the universe and beyond. Your journey to the stars starts here.</p>
+        <p>
+          Explore the universe and beyond. Your journey to the stars starts
+          here.
+        </p>
         <p>&copy; 2024 Galactica. All rights reserved.</p>
       </div>
       {/* TASK - React 1 week 2 */}
@@ -39,6 +42,9 @@ export const Footer = () => {
             <a href="https://tiktok.com">Tiktok</a>
           </li>
           <li>
+            <a href="https://linkedin.com/">LinkedIn</a>
+          </li>
+          <li>
             <a href="https://google.com">On the streets at night</a>
           </li>
           {/* TASK - React 1 week 2 */}
@@ -50,4 +56,4 @@ export const Footer = () => {
       </div>
     </footer>
   );
-}
+};
